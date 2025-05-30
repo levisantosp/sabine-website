e34b3b0c83178b136aa212db5f0247a3fbf6f562
levisantosp <contatowizard01@gmail.com>
2025-05-29 22:25:05 -0300

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,72 @@
+import Image from "next/image"
+
+export default function Home() {
+  return (
+    <>
+      <div className="px-30 pt-15 flex items-start gap-10">
+        <div>
+          <h1 className="text-7xl max-w-md">
+            Your <span className="text-[#3442b8]">e-sports</span> Discord bot
+          </h1>
+
+          <p
+            className="text-3xl max-w-lg pt-10 text-[#9CA3AF]"
+          >
+            More than just a bot, I'm your source of first-hand information and entertainment for your Discord server!
+          </p>
+
+          <ul className="text-3xl pt-10 flex flex-col gap-y-4 text-[#9CA3AF]">
+            <li className="flex gap-4">
+              <Image
+                src="/calendar.png"
+                width={30}
+                height={50}
+                alt="calendar"
+              />
+
+              <span>Schedule matches from various leagues</span>
+            </li>
+            <li className="flex gap-4">
+              <Image
+                src="/target.png"
+                width={30}
+                height={50}
+                alt="target"
+              />
+
+              <span>Stay up to date with results and statistics</span>
+            </li>
+            <li className="flex gap-4">
+              <Image
+                src="/light.png"
+                width={30}
+                height={50}
+                alt="light"
+              />
+
+              <span>Make predictions</span>
+            </li>
+            <li className="flex gap-4">
+              <Image
+                src="/medal.png"
+                width={30}
+                height={50}
+                alt="medal"
+              />
+
+              <span>Compete against the best players</span>
+            </li>
+          </ul>
+        </div>
+
+        <Image
+          src="/image.png"
+          width={579}
+          height={369}
+          alt="image"
+          className="relative top-20 left-40 rounded-xl shadow-xl filter brightness-90 transition-transform duration-500 hover:scale-125 hover:brightness-110 hover:-translate-y-1"
+        />
+      </div>
+    </>
+  )
+}
---
+++ src/components/global/Header.tsx
@@ -0,0 +1,71 @@
+import local from "next/font/local"
+import Link from "next/link"
+import Image from "next/image"
+
+const lubrifont = local({
+  src: "../../app/fonts/WDXLLubrifontTC-Regular.ttf",
+  display: "swap"
+})
+
+export default function Header({ children }: { children?: React.ReactNode }) {
+  return (
+    <>
+      <header className="bg-[#0d763e] shadow-xl">
+        <nav className="p-4 flex items-center justify-between">
+          <div className="transition duration-500 hover:scale-115">
+            <Link href="/" className="flex flex-wrap items-center justify-between px-30">
+              <Image
+                src="/sabine.png"
+                width={80}
+                height={1024}
+                alt="sabine"
+                className="rounded-full"
+              ></Image>
+
+              <span className="text-5xl font-bold px-5">Sabine</span>
+            </Link>
+          </div>
+
+          <ul className="flex flex-wrap items-center justify-between gap-5 text-3xl px-25">
+            <li className="transition duration-500 hover:scale-115">
+              <Link href="/" className="inline-block px-4 py-2 bg-[#0d763e] text-white rounded-md hover:bg-[#11924b] transition">Home</Link>
+            </li>
+
+            <li className="transition duration-500 hover:scale-115">
+              <Link href="/commands" className="inline-block px-4 py-2 bg-[#0d763e] text-white rounded-md hover:bg-[#11924b] transition">Commands</Link>
+            </li>
+
+            <li className="transition duration-500 hover:scale-115">
+              <div>
+                <a
+                href="https://discord.com/invite/FaqYcpA84r"
+                target="_blank"
+                className="flex gap-2 items-center px-4 py-2 bg-[#0d763e] text-white rounded-md hover:bg-[#11924b] transition"
+                >
+                  <span>Support and community server</span>
+                  <Image
+                    src="/redirect.png"
+                    width={30}
+                    height={50}
+                    alt="redirect"
+                    className="invert"
+                  />
+                </a>
+              </div>
+            </li>
+
+            <li>
+              {/* change lang option here */}
+            </li>
+          </ul>
+        </nav>
+      </header>
+
+      <html lang="en">
+        <body className={lubrifont.className}>
+          {children}
+        </body>
+      </html>
+    </>
+  )
+}
