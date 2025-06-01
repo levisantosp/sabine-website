2302e028087216d2bbbaab7cdf1613fae1c23da7
levisantosp <contatowizard01@gmail.com>
2025-06-01 00:19:51 -0300

    making site adapted for mobile devices

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -7,16 +7,16 @@ export default async function Home() {
 
   return (
     <>
-      <div className="px-30 pt-15 flex items-start gap-15">
+      <div className="px-4 md:px-30 pt-8 md:pt-15 flex flex-col md:flex-row items-start gap-8 md:gap-1">
         <div>
-          <h1 className="text-6xl max-w-md">
+          <h1 className="text-5xl md:text-6xl max-w-full md:max-w-md">
             {t.rich("home.title", {
               strong: (chunks) => <span className="text-[#3442b8]">{chunks}</span>
             })}
           </h1>
 
           <p
-            className="text-3xl max-w-lg pt-10 text-[#9CA3AF]"
+            className="text-2xl md:text-3xl max-w-sm md:max-w-lg pt-6 md:pt-10 text-[#9CA3AF]"
           >
             {t("home.desc.1")}
           </p>
@@ -24,7 +24,7 @@ export default async function Home() {
           <Link
             href="https://discord.com/oauth2/authorize?client_id=1235576817683922954&scope=bot&permissions=388096"
             target="_blank"
-            className="flex items-center justify-center text-4xl bg-[#3442b8] rounded-md gap-3 mt-5 w-fit px-6 py-2 transition duration-500 hover:scale-115"
+            className="flex items-center justify-center text-3xl md:text-4xl bg-[#3442b8] rounded-md gap-3 mt-5 w-fit px-6 py-2 transition duration-500 hover:scale-115"
           >
             <span>
               {t("home.add")}
@@ -39,13 +39,14 @@ export default async function Home() {
             />
           </Link>
 
-          <ul className="text-3xl pt-10 flex flex-col gap-y-4 text-[#9CA3AF]">
+          <ul className="text-xl md:text-3xl pt-10 flex flex-col gap-y-4 text-[#9CA3AF] max-w-xs md:max-w-full">
             <li className="flex gap-4">
               <Image
                 src="/calendar.png"
                 width={30}
                 height={50}
                 alt="calendar"
+                className="w-7 h-7 md:w-[30px] md:h-[35px]"
               />
 
               <span>{t("home.desc.2")}</span>
@@ -56,6 +57,7 @@ export default async function Home() {
                 width={30}
                 height={50}
                 alt="target"
+                className="w-7 h-7 md:w-[30px] md:h-[35px]"
               />
 
               <span>{t("home.desc.3")}</span>
@@ -66,6 +68,7 @@ export default async function Home() {
                 width={30}
                 height={50}
                 alt="light"
+                className="w-7 h-7 md:w-[30px] md:h-[35px]"
               />
 
               <span>{t("home.desc.4")}</span>
@@ -76,6 +79,7 @@ export default async function Home() {
                 width={30}
                 height={50}
                 alt="medal"
+                className="w-7 h-7 md:w-[30px] md:h-[35px]"
               />
 
               <span>{t("home.desc.5")}</span>
@@ -88,7 +92,7 @@ export default async function Home() {
           width={579}
           height={369}
           alt="image"
-          className="relative top-20 left-40 rounded-xl shadow-xl filter brightness-90 transition-transform duration-500 hover:scale-125 hover:brightness-110 hover:-translate-y-1"
+          className="relative md:top-20 md:left-40 mx-auto mt-8 rounded-xl shadow-xl filter brightness-90 transition-transform duration-500 hover:scale-125 hover:brightness-110 hover:-translate-y-1"
         />
       </div>
     </>
