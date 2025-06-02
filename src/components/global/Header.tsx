31a20ee325e1cd2b0ade33bd97599817da349802
levisantosp <contatowizard01@gmail.com>
2025-06-02 16:16:27 -0300

    changed header images

--- src/components/global/Header.tsx
+++ src/components/global/Header.tsx
@@ -2,6 +2,13 @@ import Link from "next/link"
 import Image from "next/image"
 import { getTranslations } from "next-intl/server"
 import LanguageMenu from "./LanguageMenu"
+import {
+  LogIn,
+  House,
+  Bot,
+  CircleHelp
+} from "lucide-react"
+import MobileMenuWrapper from "./MobileMenuWrapper"
 
 type Props = {
   params: Promise<{
@@ -15,7 +22,7 @@ export default async function Header({ params }: Props) {
   return (
     <>
       <header className="bg-[#0d763e] shadow-xl p-2 md:px-10">
-        <div className="flex flex-wrap items-center justify-between">
+        <div className="hidden md:flex items-center justify-between">
           <Link href="/" className="flex items-center md:px-20 gap-3 sm:gap-4 transition duration-300 hover:scale-105">
             <Image
               src="/header/sabine.png"
@@ -27,14 +34,17 @@ export default async function Header({ params }: Props) {
             <span className="text-xl sm:text-3xl md:text-5xl font-bold text-white">Sabine</span>
           </Link>
 
-          <ul className="flex flex-wrap items-center justify-end gap-4 text-sm sm:text-xl md:text-3xl mt-2 sm:mt-0">
+          <ul className="flex flex-wrap items-center justify-end gap-4 text-xl/6 mt-2 sm:mt-0">
             <li className="transition duration-300 hover:scale-105">
               <Link
                 href={process.env.INVITE!}
                 className="inline-block py-1 sm:py-2 px-2 sm:px-4 bg-[#3442b8] rounded-md"
                 target="_blank"
               >
-                {t("home.add")}
+                <div className="flex gap-2">
+                  <LogIn />
+                  {t("home.add")}
+                </div>
               </Link>
             </li>
 
@@ -43,16 +53,22 @@ export default async function Header({ params }: Props) {
                 href="/"
                 className="inline-block py-1 sm:py-2 px-2 sm:px-4 bg-[#0d763e] rounded-md hover:bg-[#11924b] transition"
               >
-                {t("header.home")}
+                <div className="flex gap-2">
+                  <House />
+                  {t("header.home")}
+                </div>
               </Link>
             </li>
 
             <li className="transition duration-300 hover:scale-105">
               <Link
                 href="/commands"
-                className="inline-block py-1 sm:py-2 px-2 sm:px-4 bg-[#0d763e] text-white rounded-md hover:bg-[#11924b] transition"
+                className="flex ga-p2 inline-block py-1 sm:py-2 px-2 sm:px-4 bg-[#0d763e] text-white rounded-md hover:bg-[#11924b] transition"
               >
-                {t("header.commands")}
+                <div className="flex gap-2">
+                  <Bot />
+                  {t("header.commands")}
+                </div>
               </Link>
             </li>
 
@@ -62,14 +78,8 @@ export default async function Header({ params }: Props) {
                 target="_blank"
                 className="flex items-center gap-2 py-1 sm:py-2 px-2 sm:px-4 bg-[#0d763e] text-white rounded-md hover:bg-[#11924b] transition"
               >
+                <CircleHelp />
                 <span>{t("header.support")}</span>
-                <Image
-                  src="/redirect.png"
-                  width={30}
-                  height={30}
-                  alt="redirect"
-                  className="invert w-4 sm:w-5 md:w-6"
-                />
               </a>
             </li>
 
@@ -78,6 +88,8 @@ export default async function Header({ params }: Props) {
             </li>
           </ul>
         </div>
+        
+        <MobileMenuWrapper />
       </header>
     </>
   )
