22cb4b66503534f8598a977f9dadbaf09aa0d33e
levisantosp <contatowizard01@gmail.com>
2025-06-02 16:31:51 -0300

    changed bg color

--- src/components/global/LanguageMenu.tsx
+++ src/components/global/LanguageMenu.tsx
@@ -38,11 +38,11 @@ export default function LanguageMenu() {
           leaveTo="opacity-0 translate-y-2"
         >
           <div
-            className="absolute top-12 right-0 bg-[#3442b8] rounded-md shadow-md w-50 z-20"
+            className="absolute top-12 right-0 bg-[#2A2A2A] rounded-md shadow-md w-50 z-20"
           >
             <ul>
               <li
-                className="flex items-center px-4 py-2 cursor-pointer gap-4 hover:bg-[#4f5cdb] transition"
+                className="flex items-center px-4 py-2 cursor-pointer gap-4 hover:bg-[#3A3A3A] rounded-md transition"
                 onClick={() => changeLanguage("br")}
               >
                 <Image
@@ -55,7 +55,7 @@ export default function LanguageMenu() {
               </li>
 
               <li
-                className="flex items-center px-4 py-2 cursor-pointer gap-4 hover:bg-[#4f5cdb] transition"
+                className="flex items-center px-4 py-2 cursor-pointer gap-4 hover:bg-[#3A3A3A] rounded-md transition"
                 onClick={() => changeLanguage("us")}
               >
                 <Image
