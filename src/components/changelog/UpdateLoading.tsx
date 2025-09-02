ca21b91c3dde990f0aef05124a9c1d1c2cdbb2b6
levisantosp <contatowizard01@gmail.com>
2025-09-02 02:31:16 -0300

    create loading animation

---
+++ src/components/changelog/UpdateLoading.tsx
@@ -0,0 +1,70 @@
+"use client"
+
+import Link from "next/link"
+import { useEffect, useState } from "react"
+
+type Content = {
+  lang: string
+  text: string
+}
+type Update = {
+  id: string
+  published_at: number
+  content: Content[]
+}
+type Props = {
+  locale: string
+}
+export default function UpdateLoading({ locale }: Props) {
+  console.log(process.env.API_URL)
+  const [loading, setLoading] = useState(true)
+  const [updates, setUpdates] = useState<Update[]>([])
+  useEffect(() => {
+    const fetchUpdates = async() => {
+      const data: Update[] = await (await fetch("/api/updates")).json()
+      setUpdates(data)
+      setLoading(false)
+    }
+    fetchUpdates()
+  }, [])
+  if(loading || !Array.isArray(updates)) {
+    return (
+      <>
+        <div
+          className="flex flex-col items-center justify-center pt-10"
+        >
+          {Array.from({ length: 5 }).map((_, i) => (
+            <div
+              key={i}
+              className="bg-[#2A2A2A] p-10 rounded-2xl max-w-xs md:max-w-2xl mb-6 w-[700] animate-pulse"
+            />
+          ))}
+        </div>
+      </>
+    )
+  }
+  return (
+    <>
+      <div
+        className="flex flex-col items-center justify-center pt-10"
+      >
+        {
+          updates.sort((a, b) => b.published_at - a.published_at)
+            .map((update, i) => (
+              <Link
+                key={i}
+                className="bg-[#2A2A2A] p-5 rounded-2xl max-w-xs md:max-w-2xl mb-6 w-[700] transition duration-500 hover:scale-105"
+                href={`/${locale}/changelog/v${update.id}`}
+              >
+                <h2
+                  className="text-2xl font-bold"
+                >
+                  v{update.id}
+                </h2>
+              </Link>
+            ))
+        }
+      </div>
+    </>
+  )
+}
