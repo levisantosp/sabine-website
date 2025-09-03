7a60190adc443e878f6f755c1152c864a40994b8
levisantosp <contatowizard01@gmail.com>
2025-09-02 22:04:39 -0300

    add load animation

--- src/app/[locale]/commands/page.tsx
+++ src/app/[locale]/commands/page.tsx
@@ -1,4 +1,5 @@
 import { getTranslations } from "next-intl/server"
+import CommandLoading from "../../../components/commands/CommandLoading"
 
 type Command = {
   name: string
@@ -18,31 +19,7 @@ export default async function Commands() {
           {t("commands.title")}
         </h1>
       </div>
-      <div
-        className="flex flex-col items-center justify-center pt-10"
-      >
-        {
-          commands
-            .sort((a, b) => a.name.localeCompare(b.name))
-            .map((cmd, index) => (
-              <div
-                key={index}
-                className="bg-[#2A2A2A] p-5 rounded-2xl max-w-xs md:max-w-2xl mb-6 w-[700] transition duration-500 hover:scale-105"
-              >
-                <h2
-                  className="text-2xl font-bold"
-                >
-                  /{cmd.name}
-                </h2>
-                <p
-                  className="mt-2 text-lg"
-                >
-                  {t(`commands.${cmd.name}.description`)}
-                </p>
-              </div>
-            ))
-        }
-      </div>
+      <CommandLoading/>
     </>
   )
 }
